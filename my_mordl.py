47b90e5468cc6d8a6d0ccdf93c48709a9585aaf2
ibrahimfe <ibrahiimm.f@gmail.com>
2023-04-27 13:26:02 +0700

    make the script functional so that it can be expanded

--- my_mordl.py
+++ my_mordl.py
@@ -1,25 +1,20 @@
 # my_mordl is a game where you guess a word and the code give you a feedback whethert you guessed it correctly or you misplace some of the letteracter
 import random
 import pathlib
+from string import ascii_letters
 
 
-def menu():
-    print("Hello World")
+def get_random_word():
+    word_list = pathlib.Path("word_list.txt")
+    words = [
+        word.upper()
+        for word in word_list.read_text(encoding="utf-8").split("\n")
+        if len(word) == 5 and all(letter in ascii_letters for letter in word)
+    ]
+    return random.choice(words)
 
 
-word_list = pathlib.Path("word_list.txt")
-word = random.choice(
-    [word.upper() for word in word_list.read_text(encoding="utf-8").strip().split("\n")]
-)
-print(word)
-for guess_count in range(1, 7):
-    user_guess = input(f"Guess No.{guess_count} : ").upper()
-
-    if user_guess == word:
-        print("You Win!")
-        break
-    print(f"Your guess is {6 - guess_count} more left")
-
+def show_guess(user_guess, word):
     correct_letter = {
         letter for letter, correct in zip(user_guess, word) if letter == correct
     }
@@ -30,5 +25,29 @@ for guess_count in range(1, 7):
     print("Correct Letters :", ", ".join(sorted(correct_letter)))
     print("Misplaced letter : ", ",".join(sorted(misplaced_letter)))
     print("Wrong Letter : ", ", ".join(sorted(wrong_letter)))
-else:
+
+
+def game_over(word):
     print(f"{word} is the correct word")
+
+
+def main():
+    # Pre Process
+    # get the random word
+    word = get_random_word()
+    print(word)
+    # main process when user playing the game
+    for guess_count in range(1, 7):
+        user_guess = input(f"Guess no.{guess_count} : ").upper()
+
+        show_guess(user_guess, word)
+        if user_guess == word:
+            break
+    else:
+        game_over(word)
+
+    # post procress when user get the result of the game
+
+
+if __name__ == "__main__":
+    main()
